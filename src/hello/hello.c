c8be897b109637fc44a5f38b5d9897cdc7a0afce
pavlovskii <pavlovskii>
2002-02-22 15:34:02 +0000

    Added hello program

---
+++ src/hello/hello.c
@@ -0,0 +1,10 @@
+/* $Id: hello.c,v 1.1 2002/02/22 15:34:02 pavlovskii Exp $ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	wprintf(L"Hello, world!\n");
+	return EXIT_SUCCESS;
+}
