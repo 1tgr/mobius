3bea6200228850ef35b77b859487a66b7e56c8d3
pavlovskii <pavlovskii>
2002-03-06 19:34:09 +0000

    grr

--- src/mgl/wmf.d
+++
@@ -1,5 +0,0 @@
-wmf.o: wmf.c /mn/include/os/syscall.h /mn/include/sys/types.h \
- /mn/include/os/sysdef.h /mn/include/os/defs.h /mn/include/stdio.h \
- /mn/include/sys/version.h /mn/include/stdlib.h /mn/include/limits.h \
- /mn/include/errno.h /mn/include/gl/mgl.h /mn/include/gl/wmf.h \
- wmfdefs.h
