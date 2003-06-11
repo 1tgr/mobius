42fedf378820421350eff457938f77defe7f5e35
pavlovskii <pavlovskii>
2003-06-11 20:08:05 +0000

    Removing DOS line ends

--- libc/stdlib/malloc.c
+++ libc/stdlib/malloc.c
@@ -1,4 +1,5 @@
 /*
+<<gratuitous change>>
   This is a version (aka dlmalloc) of malloc/free/realloc written by
   Doug Lea and released to the public domain.  Use, modify, and
   redistribute this code without permission or acknowledgement in any
